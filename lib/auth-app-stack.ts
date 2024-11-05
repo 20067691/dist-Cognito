828f47a92a5b3796ce0b13bc06863c4cc6c53d9b
DeanS <20067691@mail.wit.ie>
2024-11-05 22:03:09 +0000

    Signup resource

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -40,6 +40,14 @@ export class AuthAppStack extends cdk.Stack {
 
     this.auth = authApi.root.addResource("auth");
 
+         // NEW
+    this.addAuthRoute(
+      "signup",
+      "POST",
+      "SignupFn",
+      'signup.ts'
+    );
+
   }
 
   // NEW
